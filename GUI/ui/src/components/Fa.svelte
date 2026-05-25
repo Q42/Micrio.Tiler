98d7dbf366a4dc8917c74b3a9973bdf00b1cdde8
Marcel Duin <duin.marcel@gmail.com>
2026-05-25 16:31:00 +0200

    binary compilation works

---
+++ GUI/ui/src/components/Fa.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { IconDefinition } from '@fortawesome/free-solid-svg-icons';
+
+	export let icon: IconDefinition;
+	export let style: string = '';
+
+	$: [w, h, , , d] = icon.icon;
+</script>
+
+<svg
+	class={$$props.class || ''}
+	{style}
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 0 {w} {h}"
+	width="1em"
+	height="1em"
+	fill="currentColor"
+>
+	<path {d} />
+</svg>
